124146ca2750bc70ce2511ffed529a8cb456ea71
Александр Дубровин <xah799@mail.ru>
2023-08-05 15:52:18 +0300

    Created project

---
+++ src/main/java/com/example/mycompany/MyCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycompany/MyCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
