de209cd7ff8229894bd0c049fcfab851db80f59a
Александр Дубровин <xah799@mail.ru>
2023-08-05 16:48:13 +0300

    Created entity Employee

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,21 +1,27 @@
 package model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "employee")
 public class Employee {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String surName;
     private LocalDate dateOfBirth;
+    @Transient
     private int age;
     private BigDecimal salary;
 
@@ -25,5 +31,9 @@ public class Employee {
         this.dateOfBirth = dateOfBirth;
         this.age = age;
         this.salary = salary;
+    }
+
+    public int getAge() {
+        return Period.between(this.dateOfBirth, LocalDate.now()).getYears();
     }
 }
